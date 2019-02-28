36945ccfa9ca2e81d372d8af7680484bab3ae878
Jacob Angulo <jacobangulo@hotmail.com>
2019-02-27 21:51:50 -0800

    hosting app on netlify so i can register for merriam websters api

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
 import React, { Component } from "react";
+import axios from 'axios';
+import WordOfTheDay from './components/WordOfTheDay';
 import "./App.css";
 
 class App extends Component {
+  state = {
+    words: []
+  }
+
+  componentDidMount() {
+    axios
+      .get('https://www.dictionaryapi.com/api/v3/references/collegiate/json/voluminous?key=your-api-key')
+      .then(res => console.log(res))
+  }
+
   render() {
     return (
-      <h1>Hello World!</h1>
+      <WordOfTheDay/>
     )
   }
 }
---
+++ src/components/WordOfTheDay.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const WordOfTheDay = () => {
+    return (
+        <p>word of the day</p>
+    )
+}
+
+export default WordOfTheDay
